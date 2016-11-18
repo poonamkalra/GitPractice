2478d222b5f1a69e601beebb9e271fc1963cdcc9
Poonam Kalra <poonam.kalra@impetus.co.in>
2016-11-18 15:20:57 +0530

    This is my first commit

---
+++ Patterns/Patterns/BasicDesignPattern.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32.SafeHandles;
+
+namespace Patterns
+{
+    //https://msdn.microsoft.com/en-us/library/b1yfkh5e(v=vs.110).aspx#basic_pattern
+    class BasicDesignPattern: IDisposable       
+    {   
+        private SafeFileHandle resource;
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);            
+        }
+
+        public virtual void Dispose(bool disposing)
+        {
+            if(disposing)
+            {
+
+            }
+        }
+    }
+}
